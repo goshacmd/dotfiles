5d29af38a3f747a0fdf31916c81d2b32d4048715
Gosha Arinich <me@goshakkk.name>
2013-07-23 14:48:22 +0300

    nested keys in slate config

--- slate.js
+++ slate.js
@@ -2,33 +2,74 @@
 //
 var extend = function(what, withWhat) { return _.extend(_.clone(what), withWhat); };
 
+var mapKeys = function(_map, mapper) {
+  var map = {};
+
+  for(key in _map) {
+    var newKey = mapper(key);
+    map[newKey] = _map[key];
+  };
+
+  return map;
+};
+
+var prefixKeysWith = function(_map, prefix) {
+  return mapKeys(_map, function(key) {
+    return '' + prefix + key;
+  });
+};
+
+var suffixKeysWith = function(_map, suffix) {
+  return mapKeys(_map, function(key) {
+    return '' + key + suffix;
+  });
+};
+
+var flattenMapping = function(mapping, mapper) {
+  var flatMap = {};
+
+  for(key in mapping) {
+    var value = mapping[key];
+
+    if (value && typeof(value) == 'object' && Object.keys(value).length > 0) {
+      var newValue = mapper(value, key);
+
+      flatMap = extend(flatMap, newValue);
+    } else {
+      flatMap[key] = value;
+    }
+  };
+
+  return flatMap;
+};
+
 /*
  * Bind all mappings.
  *
- * prefixMap - an optional object of key prefix mappings.
- * bindMap   - an object of key -> operation mappings.
+ * aliases - an object of alias -> real mappings.
+ * bindMap - an object of key -> operation/object mappings.
  *
- * In Slate config (non-JS), you can just "bind a:${someprefix} relaunch".
- * In JS version of config, however, you cannot, because you can't use
- * expressions in the key part of object literal in JS, it becomes rather
- * cumbersome to repeat same prefix in every mapping. To address these
- * issues, you can pass an object of prefix mappings to `bindAll` along
- * with the bind mappings. In bind mappings then, you can use hash symbol
- * and name of your prefix as a placeholder.
+ * Examples
+ *
+ * bindAll({
+ *   hyper: 'h'
+ * }, {
+ *   a: 1,
+ *   b: { c: 2 },
+ *   hyper: { d: 3 }
+ * });
+ * => { a: 1, 'c:b': 2, 'd:h': 3 }
  */
-var bindAll= function(prefixMap, _bindMap) {
-  bindMap = {};
+var bindAll = function(aliases, _bindMap) {
+  var aliasedMap = flattenMapping(_bindMap, function(map, key) {
+    return suffixKeysWith(map, ':' + key);
+  });
 
-  for(key in _bindMap) {
-    var match, newKey = key;
-
-    if (match = key.match(/.*:\s*#(.*)/)) {
-      var val = prefixMap[match[1]];
-      newKey = key.replace("#" + match[1], val).replace(/\s+/, '');
-    }
-
-    bindMap[newKey] = _bindMap[key];
-  };
+  var bindMap = mapKeys(aliasedMap, function(key) {
+    return _.reduce(aliases, function(memo, map, alias) {
+      return memo.replace(alias, map);
+    }, key);
+  });
 
   // Bind it, finally.
   S.bnda(bindMap);
@@ -173,82 +214,99 @@ var laptop = S.op("layout", { name: laptopLayout }),
 
 // Bind everything
 bindAll({
-  // Binding prefixes
-  layoutKeys: "ctrl;cmd",
-  locationKeys: "ctrl;cmd",
-  resizeKeys1: "cmd;alt",
-  resizeKeys2: "ctrl;alt",
-  pushKeys: "ctrl;cmd",
-  throwKeys: "ctrl;alt",
-  nudgeKeys: "ctrl;alt;cmd",
-  focusKeys: "ctrl;shift;alt;cmd"
+  layoutKeys: 'ctrl;cmd',
+  layoutKeys: 'ctrl;cmd',
+  locationKeys: 'ctrl;cmd',
+  resizeKeys1: 'cmd;alt',
+  resizeKeys2: 'ctrl;alt',
+  pushKeys: 'ctrl;cmd',
+  throwKeys: 'ctrl;alt',
+  nudgeKeys: 'ctrl;alt;cmd',
+  focusKeys: 'ctrl;shift;alt;cmd',
 }, {
-  // Layout bindings
-  "l:#layoutKeys": laptop,
-  "t:#layoutKeys": thunderbolt,
-  "b:#layoutKeys": twoMonitor,
-  "u:#layoutKeys": universalLayout,
+  layoutKeys: {
+    // Layout bindings
+    l: laptop,
+    t: thunderbolt,
+    b: twoMonitor,
+    u: universalLayout,
+  },
 
-  // Location bindings
-  "0:#locationKeys": lapChat,
-  "[:#locationKeys": lapSocial,
-  ";:#locationKeys": lapFull,
-  "1:#locationKeys": lapAndTbolt(lapFull, tboltFull),
-  "2:#locationKeys": tboltKindaFull,
-  "3:#locationKeys": tboltLeft,
-  "4:#locationKeys": tboltKindaLeft,
-  "5:#locationKeys": tboltRight,
-  "6:#locationKeys": tboltKindaRight,
-  "7:#locationKeys": lapAndTbolt(lapChat, tboltChat),
-  "8:#locationKeys": lapAndTbolt(lapSocial, tboltSocialTop),
-  "9:#locationKeys": lapAndTbolt(lapSocial, tboltSocialBot),
+  locationKeys: {
+    // Location bindings
+    "0": lapChat,
+    "[": lapSocial,
+    ";": lapFull,
+    "1": lapAndTbolt(lapFull, tboltFull),
+    "2": tboltKindaFull,
+    "3": tboltLeft,
+    "4": tboltKindaLeft,
+    "5": tboltRight,
+    "6": tboltKindaRight,
+    "7": lapAndTbolt(lapChat, tboltChat),
+    "8": lapAndTbolt(lapSocial, tboltSocialTop),
+    "9": lapAndTbolt(lapSocial, tboltSocialBot),
+  },
 
-  // Resize bindings
-  "right: #resizeKeys1": S.op("resize", { width: "+10%", height: "+0" }),
-  "left:  #resizeKeys1": S.op("resize", { width: "-10%", height: "+0" }),
-  "up:    #resizeKeys1": S.op("resize", { width: "+0", height: "-10%" }),
-  "left:  #resizeKeys1": S.op("resize", { width: "+0", height: "+10%" }),
-  "right: #resizeKeys2": S.op("resize", { width: "-10%", height: "+0", anchor: "bottom-right" }),
-  "left:  #resizeKeys2": S.op("resize", { width: "+10%", height: "+0", anchor: "bottom-right" }),
-  "up:    #resizeKeys2": S.op("resize", { width: "+0", height: "+10%", anchor: "bottom-right" }),
-  "down:  #resizeKeys2": S.op("resize", { width: "+0", height: "-10%", anchor: "bottom-right" }),
+  resizeKeys1: {
+    // Resize bindings
+    right: S.op("resize", { width: "+10%", height: "+0" }),
+    left:  S.op("resize", { width: "-10%", height: "+0" }),
+    up:    S.op("resize", { width: "+0", height: "-10%" }),
+    left:  S.op("resize", { width: "+0", height: "+10%" }),
+  },
 
-  // Push bindings
-  "/:     #pushKeys": S.op("move", { x: "screenSizeX/4", y: 0, width: "screenSizeX/2", height: "screenSizeY" }), // center
-  "n:     #pushKeys": S.op("move", { x: "screenSizeX/8", y: 0, width: "3*screenSizeX/4", height: "screenSizeY" }), // normal
-  "r:     #pushKeys": S.op("move", { x: 0, y: 0, width: "screenSizeX", height: "screenSizeY" }), // full-screen
-  "right: #pushKeys": S.op("push", { direction: "right", style: "bar-resize:screenSizeX/3" }),
-  "left:  #pushKeys": S.op("push", { direction: "left", style: "bar-resize:screenSizeX/3" }),
-  "down:  #pushKeys": S.op("push", { direction: "down", style: "bar-resize:screenSizeX/2" }),
-  "up:    #pushKeys": S.op("push", { direction: "up", style: "bar-resize:screenSizeX/2" }),
+  resizeKeys2: {
+    right: S.op("resize", { width: "-10%", height: "+0", anchor: "bottom-right" }),
+    left:  S.op("resize", { width: "+10%", height: "+0", anchor: "bottom-right" }),
+    up:    S.op("resize", { width: "+0", height: "+10%", anchor: "bottom-right" }),
+    down:  S.op("resize", { width: "+0", height: "-10%", anchor: "bottom-right" }),
+  },
 
-  // Nudge bindings
-  "right: #nudgeKeys": S.op("nudge", { x: "+5%", y: "+0" }),
-  "left:  #nudgeKeys": S.op("nudge", { x: "-5%", y: "+0" }),
-  "down:  #nudgeKeys": S.op("nudge", { x: "+0", y: "+5%" }),
-  "up:    #nudgeKeys": S.op("nudge", { x: "+0", y: "-5%" }),
+  pushKeys: {
+    // Push bindings
+    "/":   S.op("move", { x: "screenSizeX/4", y: 0, width: "screenSizeX/2", height: "screenSizeY" }), // center
+    n:     S.op("move", { x: "screenSizeX/8", y: 0, width: "3*screenSizeX/4", height: "screenSizeY" }), // normal
+    r:     S.op("move", { x: 0, y: 0, width: "screenSizeX", height: "screenSizeY" }), // full-screen
+    right: S.op("push", { direction: "right", style: "bar-resize:screenSizeX/3" }),
+    left:  S.op("push", { direction: "left", style: "bar-resize:screenSizeX/3" }),
+    down:  S.op("push", { direction: "down", style: "bar-resize:screenSizeX/2" }),
+    up:    S.op("push", { direction: "up", style: "bar-resize:screenSizeX/2" }),
+  },
 
-  // Throw bindings
-  "1:#throwKeys": S.op("throw", { screen: monLaptop, width: "screenSizeX", height: "screenSizeY" }), // throw to Air screen & make full-screen
-  "2:#throwKeys": S.op("throw", { screen: monTbolt, width: "screenSizeX", height: "screenSizeY" }), // throw to TD & make full-screen
-  "3:#throwKeys": S.op("throw", { screen: monLaptop }), // throw to Air screen but preserve window size
-  "4:#throwKeys": S.op("throw", { screen: monTbolt }), // throw to TD but preserve window size
+  nudgeKeys: {
+    // Nudge bindings
+    right: S.op("nudge", { x: "+5%", y: "+0" }),
+    left:  S.op("nudge", { x: "-5%", y: "+0" }),
+    down:  S.op("nudge", { x: "+0", y: "+5%" }),
+    up:    S.op("nudge", { x: "+0", y: "-5%" }),
+  },
 
-  // Focus bindings
-  "s:#focusKeys": S.op("focus", { app: "Safari" }), // *S*afari
-  "t:#focusKeys": S.op("focus", { app: "iTerm" }), // i*T*erm
-  "i:#focusKeys": S.op("focus", { app: "iTunes" }), // *i*Tunes
-  "x:#focusKeys": S.op("focus", { app: "Xcode" }), // *X*code
-  "d:#focusKeys": S.op("focus", { app: "Cheddar" }), // Che*d*dar
-  "v:#focusKeys": S.op("focus", { app: "Preview" }), // Pre*v*iew
-  "l:#focusKeys": S.op("focus", { app: "VLC" }), // V*L*C
-  "m:#focusKeys": S.op("focus", { app: "Messages" }), // *M*essages
-  "k:#focusKeys": S.op("focus", { app: "Skype" }), // S*k*ype
-  "b:#focusKeys": S.op("focus", { app: "Tweetbot" }), // Tweet*b*ot
-  "w:#focusKeys": S.op("focus", { app: "Wedge" }), // *W*edge
-  "p:#focusKeys": S.op("focus", { app: "Sparrow" }), // S*p*arrow
-  "c:#focusKeys": S.op("focus", { app: "Google Chrome" }), // Google *C*hrome
-  "f:#focusKeys": S.op("focus", { app: "Finder" }), // *F*inder
+  throwKeys: {
+    // Throw bindings
+    "1": S.op("throw", { screen: monLaptop, width: "screenSizeX", height: "screenSizeY" }), // throw to Air screen & make full-screen
+    "2": S.op("throw", { screen: monTbolt, width: "screenSizeX", height: "screenSizeY" }), // throw to TD & make full-screen
+    "3": S.op("throw", { screen: monLaptop }), // throw to Air screen but preserve window size
+    "4": S.op("throw", { screen: monTbolt }), // throw to TD but preserve window size
+  },
+
+  focusKeys: {
+    // Focus bindings
+    s: S.op("focus", { app: "Safari" }), // *S*afari
+    t: S.op("focus", { app: "iTerm" }), // i*T*erm
+    i: S.op("focus", { app: "iTunes" }), // *i*Tunes
+    x: S.op("focus", { app: "Xcode" }), // *X*code
+    d: S.op("focus", { app: "Cheddar" }), // Che*d*dar
+    v: S.op("focus", { app: "Preview" }), // Pre*v*iew
+    l: S.op("focus", { app: "VLC" }), // V*L*C
+    m: S.op("focus", { app: "Messages" }), // *M*essages
+    k: S.op("focus", { app: "Skype" }), // S*k*ype
+    b: S.op("focus", { app: "Tweetbot" }), // Tweet*b*ot
+    w: S.op("focus", { app: "Wedge" }), // *W*edge
+    p: S.op("focus", { app: "Sparrow" }), // S*p*arrow
+    c: S.op("focus", { app: "Google Chrome" }), // Google *C*hrome
+    f: S.op("focus", { app: "Finder" }), // *F*inder
+  },
 
   // Etc
   "esc:cmd": S.op("hint"), // window hints
